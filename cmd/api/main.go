740f5e51dc67c42d31b1b388049c7334f140b682
Xanssun <115156090+Xanssun@users.noreply.github.com>
2024-10-10 00:22:02 +0500

    database con

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/Xanssun/gopherSocial.git/internal/db"
 	"github.com/Xanssun/gopherSocial.git/internal/env"
 	"github.com/Xanssun/gopherSocial.git/internal/store"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -16,9 +19,29 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr: env.GetString("DB_ADDR",
+				"postgres://user:adminpassword@localhost:5432/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_CONNS", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Printf("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
